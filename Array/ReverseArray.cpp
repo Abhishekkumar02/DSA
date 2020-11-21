99650aeaaab9c8fa28f32f51c1ab95b88b3fce64
Abhishek <abhishek.kumar@thoughts2binary.com>
2020-11-22 01:41:33 +0530

    Added array program reverse array

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <iostream> 
+using namespace std;
+ 
+void rvereseArray(int arr[], int start, int end)
+{
+    while (start < end)
+    {
+        int temp = arr[start]; 
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}     
+
+void printArray(int arr[], int size)
+{
+   for (int i = 0; i < size; i++)
+   cout << arr[i] << " ";
+ 
+   cout << endl;
+} 
+int main() 
+{
+    int n,arr[10];
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    // int n = sizeof(arr) / sizeof(arr[0]); 
+
+    printArray(arr, n);
+    rvereseArray(arr, 0, n-1);
+     
+    cout << "Reversed array is" << endl;
+    printArray(arr, n); 
+    return 0;
+}
