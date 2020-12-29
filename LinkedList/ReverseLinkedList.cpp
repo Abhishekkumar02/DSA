e906a10dbcfbaf44d49657ccd1185b30271a7afb
Abhishek <abhishek.kumar@thoughts2binary.com>
2020-12-29 22:35:03 +0530

    Adding linkedlist program

---
+++ LinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+struct node{
+    int data;
+    struct node* next;
+    node(int value){
+        data = value;
+        next = NULL;
+    }
+};
+
+struct node* reverse(struct node* head){
+    struct node *curr = head;
+    struct node *prev=NULL,*next=NULL;
+    while(curr!=NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev=curr;
+        curr=next;
+    }
+    head = prev;
+    return head;
+}
+
+void printList(struct node* head){
+
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<"\n";
+}
+int main(){
+    int n;
+    cin>>n;
+    struct node* temp=NULL;
+        struct node* head=NULL;
+    for(int i=0;i<n;i++){
+        int val;
+        cin >>val;
+        if(i==0){
+            head = new node(val);
+            temp = head;
+
+        }else{
+            temp->next = new node(val);
+            temp = temp->next;
+        }
+
+    }
+    printList(head);
+    head = reverse(head);
+    printList(head);
+return 0;
+}
